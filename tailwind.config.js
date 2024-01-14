f38147517593c42839673f510c1cb143ac7d5180
ROG <luan.roger.2003@gmail.com>
2024-01-13 21:04:27 -0300

    feat: add the Geist font

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,9 @@ module.exports = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontFamily: {
+        sans: ["Geist", "sans-serif"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
