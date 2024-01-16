63dff8af888d846cdd5765ff17a66e6c57a3e468
ROG <luan.roger.2003@gmail.com>
2024-01-16 10:50:48 -0300

    feat: create `BaseLayout`

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DragWindowRegion from "../components/DragWindowRegion";
+
+export default function BaseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+    <DragWindowRegion />
+    <main>{children}</main>
+    </>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
 export default function HomePage() {
-    return (
-        <div>
-            <h1
-            className="text-blue-700"
-            >This is the Home Page</h1>
-        </div>
-    )
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <h1 className="text-4xl font-bold">Home Page</h1>
+      </div>
+    </>
+  );
 }
