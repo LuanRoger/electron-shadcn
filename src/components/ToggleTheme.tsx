2be255eb9a2aaada3d74ebc6a697cf8c7cc12422
ROG <luan.roger.2003@gmail.com>
2024-07-03 10:28:36 -0300

    feat: save the current theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,14 +1,9 @@
 import { Moon } from "lucide-react";
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { updateDocumentTheme } from "@/helpers/theme_helpers";
+import { toggleTheme } from "@/helpers/theme_helpers";
 
 export default function ToggleTheme() {
-    async function toggleTheme() {
-        const isDarkMode = await window.themeMode.toggle();
-        updateDocumentTheme(isDarkMode);
-    }
-
     return (
         <Button onClick={toggleTheme} size="icon">
             <Moon size={16} />
