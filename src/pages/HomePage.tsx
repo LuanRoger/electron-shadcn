58e8cc92dd2b03bbb8a8d6aad86f538acb2dd0d8
ROG <luan.roger.2003@gmail.com>
2025-01-19 16:04:23 -0300

    feat: create Footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import ToggleTheme from "@/components/ToggleTheme";
 import { useTranslation } from "react-i18next";
 import LangToggle from "@/components/LangToggle";
+import Footer from "@/components/temp/Footer";
 
 export default function HomePage() {
   const { t } = useTranslation();
 
   return (
-    <>
-      <div className="flex h-screen flex-col items-center justify-center gap-2">
+    <div className="flex h-full flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center gap-2">
         <h1 className="text-4xl font-bold">{t("title")}</h1>
         <LangToggle />
         <ToggleTheme />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
