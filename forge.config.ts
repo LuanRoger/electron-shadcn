40f2f74ba22267dca8cea7b4b46ea48b95b4c03b
ROG <luan.roger.2003@gmail.com>
2025-04-28 10:17:32 -0300

    fix: remove package.json from import

--- forge.config.ts
+++ forge.config.ts
@@ -6,12 +6,9 @@ import { MakerRpm } from "@electron-forge/maker-rpm";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
-import pkg from "./package.json";
 
 const config: ForgeConfig = {
   packagerConfig: {
-    executableName: pkg.name, 
-    name: pkg.productName, 
     asar: true,
   },
   rebuildConfig: {},
@@ -23,11 +20,8 @@ const config: ForgeConfig = {
   ],
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
       build: [
         {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
           entry: "src/main.ts",
           config: "vite.main.config.ts",
           target: "main",
@@ -45,8 +39,7 @@ const config: ForgeConfig = {
         },
       ],
     }),
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
+
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
