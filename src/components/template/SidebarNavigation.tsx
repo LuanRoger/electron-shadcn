e7840c385e609e1d7f0ae4fe0a4fe292740d3993
Matthias Rieck <Matthias.Rieck@icloud.com>
2025-06-19 08:57:00 +0200

    Scrollable

--- src/components/template/SidebarNavigation.tsx
+++ src/components/template/SidebarNavigation.tsx
@@ -69,12 +69,12 @@ export function SidebarLayout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main className="flex-1 flex flex-col min-h-screen">
+            <main className="flex-1 flex flex-col h-screen">
                 <div className="flex items-center p-2 border-b">
                     <SidebarTrigger />
                     <h1 className="ml-2 font-semibold">Expenses</h1>
                 </div>
-                <div className="flex-1 p-4">
+                <div className="flex-1 p-4 overflow-auto">
                     {children}
                 </div>
             </main>
