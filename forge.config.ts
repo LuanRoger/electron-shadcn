b4aa319adc34378919f0a81221ff13d17e4f0294
ROG <luan.roger.2003@gmail.com>
2025-08-04 08:35:00 -0300

    fix: type for target parse errors

--- forge.config.ts
+++ forge.config.ts
@@ -23,12 +23,12 @@ const config: ForgeConfig = {
       build: [
         {
           entry: "src/main.ts",
-          config: "vite.main.config.ts",
+          config: "vite.main.config.mts",
           target: "main",
         },
         {
           entry: "src/preload.ts",
-          config: "vite.preload.config.ts",
+          config: "vite.preload.config.mts",
           target: "preload",
         },
       ],
