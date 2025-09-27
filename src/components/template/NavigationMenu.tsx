6c5d43c532b32d5e78e7a3795dcc7a297517ac93
ROG <luan.roger.2003@gmail.com>
2025-09-27 18:11:33 -0300

    fix: update routes impors and paths

--- src/components/template/NavigationMenu.tsx
+++ src/components/template/NavigationMenu.tsx
@@ -22,7 +22,7 @@ export default function NavigationMenu() {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link to="/second-page">{t("titleSecondPage")}</Link>
+            <Link to="/second">{t("titleSecondPage")}</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
