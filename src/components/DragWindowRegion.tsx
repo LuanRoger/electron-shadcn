aa67a27273f45e4b4ecf4fb59bfb23857432993a
ROG <luan.roger.2003@gmail.com>
2025-11-09 20:53:08 -0300

    style: run Prettier

--- src/components/DragWindowRegion.tsx
+++ src/components/DragWindowRegion.tsx
@@ -1,8 +1,4 @@
-import {
-  closeWindow,
-  maximizeWindow,
-  minimizeWindow,
-} from "@/actions/window";
+import { closeWindow, maximizeWindow, minimizeWindow } from "@/actions/window";
 import { isMacOS } from "@/utils/platform";
 import { type ReactNode } from "react";
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,4 +5,4 @@ export const LOCAL_STORAGE_KEYS = {
 
 export const IPC_CHANNELS = {
   START_ORPC_SERVER: "start-orpc-server",
-}
+};
