4a0c4472e51635948268a612a43491ac578cde14
ROG <luan.roger.2003@gmail.com>
2026-03-22 16:24:07 -0300

    feat: show TanStackRouterDevtools only when in development mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,16 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Activity } from "react";
+import { inDevelopment } from "@/constants";
 import BaseLayout from "@/layouts/base-layout";
-
-/* import { TanStackRouterDevtools } from '@tanstack/react-router-devtools' */
-
-/*
- * Uncomment the code in this file to enable the router devtools.
- */
 
 function Root() {
   return (
     <BaseLayout>
       <Outlet />
-      {/* Uncomment the following line to enable the router devtools */}
-      {/* <TanStackRouterDevtools /> */}
+      <Activity mode={inDevelopment ? "visible" : "hidden"}>
+        <TanStackRouterDevtools />
+      </Activity>
     </BaseLayout>
   );
 }
